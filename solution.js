e2e01fc6983bc2d85cac61be13a225172712bc4d
shweta <sry9281@gmail.com>
2020-01-22 18:26:54 +0530

    Added find project method

--- solution.js
+++ solution.js
@@ -3,7 +3,7 @@ const url = 'mongodb://localhost:27017/learnyoumongo';
 const age = +process.argv[2];   
 mongo.connect(url, function(err, db) {
   let data =db.db("learnyoumongo");
-  (data.collection('parrots').find({age : {$gt: age}})).toArray(function(err, documents){
+  (data.collection('parrots').find({age : {$gt: age}},{projection:{name: 1, age: 1, _id: 0}})).toArray(function(err, documents){
     if(err) throw err;
     console.log(documents);
        
