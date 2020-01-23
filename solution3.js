01842f96090ee675a865bd8ba12b6961b82a506e
shweta <sry9281@gmail.com>
2020-01-23 10:49:55 +0530

    Remove function is added

--- solution3.js
+++ solution3.js
@@ -3,6 +3,7 @@ const url = 'mongodb://localhost:27017/learnyoumongo';
 const dbName = process.argv[2];  
 mongo.connect(url, function(err, db) {
   let data =db.db(dbName);
+  //update function
   data.collection('users').update({
     username: "tinatime"
   },{
@@ -11,6 +12,12 @@ mongo.connect(url, function(err, db) {
     }
   },function(err){
     if (err) throw err;
+   })
+   //remove function
+   data.collection(process.argv[3]).remove({
+     _id: process.argv[4]
+   },function(err){
+     if(err) throw err;
    })
  db.close();
 })
